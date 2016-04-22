3c3ccf42efa48b6f093175b4b1443f328300e038
Sanu Ann <abraham.sanuann@gmail.com>
2016-04-22 10:38:17 -0400

    SA: Reformat code, added polyline distance.

--- app/src/main/java/com/example/jobin/mapdraw/MapsActivity.java
+++ app/src/main/java/com/example/jobin/mapdraw/MapsActivity.java
@@ -1,15 +1,19 @@
 package com.example.jobin.mapdraw;
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat.OnRequestPermissionsResultCallback;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.KeyEvent;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
@@ -21,51 +25,76 @@ import android.widget.TextView.OnEditorActionListener;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
 import com.google.android.gms.maps.GoogleMap.OnMapLongClickListener;
 import com.google.android.gms.maps.GoogleMap.OnMarkerDragListener;
+import com.google.android.gms.maps.GoogleMap.OnPolylineClickListener;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.maps.android.SphericalUtil;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements
-        OnMapReadyCallback, OnMapClickListener, OnMapLongClickListener {
+        OnMapReadyCallback, OnMapClickListener, OnMapLongClickListener,
+        OnRequestPermissionsResultCallback {
+
+    //Request code for location permission request.
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+
+    //Flag indicating whether a requested permission has been denied after returning
+    private boolean mPermissionDenied = false;
 
     private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+
+    // Root of the layout of this Activity.
+    //private View view;
+
     private static final String TAG = "MapsActivity";
+    private double distance;
     private boolean placeMarkerFlag;
     private GoogleMap mMap;
     private ArrayList<LatLng> arrayPoints;
     private ArrayList<ArrayList<LatLng>> arrayPointsList;
     private PolylineOptions polylineOptions;
+    private Polyline polyline;
     private MarkerOptions markerOptions;
     private LatLng latLng;
 
+    private RadioGroup rgViews;
+    private Button placeMarker;
+    private Button drawLine;
+    private EditText etLocation;
+    private TextView polyline_distance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        //view = findViewById(R.id.main_layout);
+
         arrayPoints = new ArrayList<LatLng>();
         arrayPointsList = new ArrayList<>();
 
         // get handle to the fragment and pass it to resource id of the <fragment> element
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-
+                .findFragmentById(R.id.map_fragment);
         // set the callback on the fragment
         mapFragment.getMapAsync(this);
 
-
-        // Getting reference to editText of the layout activity_maps
-        final EditText etLocation = (EditText) findViewById(R.id.et_location);
+        /* Getting reference to UI bits in activity_maps */
+        etLocation = (EditText) findViewById(R.id.et_location);
+        rgViews = (RadioGroup) findViewById(R.id.rg_views);
+        placeMarker = (Button) findViewById(R.id.btn_marker);
+        drawLine = (Button) findViewById(R.id.btn_line);
+        polyline_distance = (TextView) findViewById(R.id.line_distance);
 
         // Defining editText search event listener for the editText
         OnEditorActionListener findClickListener = new TextView.OnEditorActionListener() {
@@ -91,75 +120,42 @@ public class MapsActivity extends FragmentActivity implements
         // Setting editText search click event listener for the keyboard search icon
         etLocation.setOnEditorActionListener(findClickListener);
 
+        setMapTypeOptions();
+        setPlaceMarkerListener();
+        setDrawLineListener();
 
-        // Getting reference to RadioGroup of the layout activity_maps and set map type selected
-        RadioGroup rgViews = (RadioGroup) findViewById(R.id.rg_views);
-        rgViews.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                if (checkedId == R.id.rb_normal) {
-                    mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-                } else if (checkedId == R.id.rb_hybrid) {
-                    mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-                } else if (checkedId == R.id.rb_satellite) {
-                    mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-                } else if (checkedId == R.id.rb_terrain) {
-                    mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
-                }
-            }
-        });
-
-        // Getting reference to place marker and draw line button and set corresponding flag
-        Button place_marker = (Button) findViewById(R.id.btn_marker);
-        place_marker.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                placeMarkerFlag = true;
-            }
-
-        });
-
-        Button draw_line = (Button) findViewById(R.id.btn_line);
-        draw_line.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                // Save and Reset last drawn line, if it exists
-                if(arrayPoints != null && arrayPoints.size() > 0){
-                    arrayPointsList.add(arrayPoints);
-                    arrayPoints = new ArrayList<LatLng>();
-                }
-                placeMarkerFlag = false;
-            }
-
-        });
     }
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
         if (checkPlayServices()) {
             mMap = googleMap;
+
+            enableMyLocation();
+
             // Sets default map type to be "hybrid"
             mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-            //mMap.setIndoorEnabled(true);
-            //mMap.setTrafficEnabled(true);
-            // 3D building
-            //mMap.setBuildingsEnabled(true);
-            // Get zoom button
+
             mMap.getUiSettings().setZoomControlsEnabled(true);
+
             mMap.setOnMapClickListener(this);
             mMap.setOnMapLongClickListener(this);
+            mMap.setOnPolylineClickListener(new OnPolylineClickListener() {
+                @Override
+                public void onPolylineClick(Polyline polyline) {
+                    distance = SphericalUtil.computeLength(polyline.getPoints());
+                    polyline_distance.setText("Distance: " +
+                            formatNumber(distance));
+                }
+            });
+
             mMap.setOnMarkerDragListener(new OnMarkerDragListener() {
 
                 @Override
                 public void onMarkerDragStart(Marker marker) {
+                    // simulate long click to delete marker
+                    //marker.remove();
                     // TODO Auto-generated method stub
                 }
 
@@ -186,17 +182,122 @@ public class MapsActivity extends FragmentActivity implements
         }
     }
 
+    // Enables the My Location layer if the fine location permission has been granted.
+    private void enableMyLocation() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            Log.i(TAG, "LOCATION permission has NOT been granted.");
+
+            // Permission to access the location is missing.
+            PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE,
+                    Manifest.permission.ACCESS_FINE_LOCATION, true);
+            //requestLocationPermission();
+
+        } else {
+            Log.v(TAG, "LOCATION permission has BEEN granted. Enabling MyLocation");
+            if (mMap != null) {
+                // Access to the location has been granted to the app.
+                mMap.setMyLocationEnabled(true);
+                Log.i(TAG, "location enabled");
+            }
+        }
+    }
+
+
+    /*
+     * Callback received when a permissions request has been completed.
+    */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            // Received permission result for location permission.
+            Log.i(TAG, "Received response for Location permission request.");
+
+            if (PermissionUtils.isPermissionGranted(permissions, grantResults,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                // Enable the my location layer if the permission has been granted.
+                enableMyLocation();
+            } else {
+                // Display the missing permission error dialog when the fragments resume.
+                mPermissionDenied = true;
+            }
+
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    @Override
+    protected void onResumeFragments() {
+        super.onResumeFragments();
+        if (mPermissionDenied) {
+            // Permission was not granted, display error dialog.
+            showMissingPermissionError();
+            mPermissionDenied = false;
+        }
+    }
+
+    //Displays a dialog with error message explaining that the location permission is missing.
+    private void showMissingPermissionError() {
+        PermissionUtils.PermissionDeniedDialog
+                .newInstance(true).show(getSupportFragmentManager(), "dialog");
+    }
+
+
+    private void setMapTypeOptions() {
+        rgViews.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.rb_normal) {
+                    mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                } else if (checkedId == R.id.rb_hybrid) {
+                    mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                } else if (checkedId == R.id.rb_satellite) {
+                    mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                } else if (checkedId == R.id.rb_terrain) {
+                    mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+                }
+            }
+        });
+    }
+
+    private void setPlaceMarkerListener() {
+        placeMarker.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                placeMarkerFlag = true;
+            }
+
+        });
+    }
+
+    private void setDrawLineListener() {
+        drawLine.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                // Save and Reset last drawn line, if it exists
+                if (arrayPoints != null && arrayPoints.size() > 0) {
+                    arrayPointsList.add(arrayPoints);
+                    arrayPoints = new ArrayList<LatLng>();
+                }
+                placeMarkerFlag = false;
+            }
+
+        });
+    }
+
+
     @Override
     public void onMapClick(final LatLng point) {
 
-        if (placeMarkerFlag) { // place marker only
+        if (placeMarkerFlag) {
+            // place marker only
             drawMarker(point);
-            //mMap.setOnMarkerDragListener(this);
-        }
-
-        // draw lines to connect the points on map
-        else {
-
+        } else {
+            // draw lines to connect the points on map
             drawLine(point);
         }
 
@@ -216,7 +317,7 @@ public class MapsActivity extends FragmentActivity implements
         marker.title("Marker Coordinates");
 
         // Coordinates for this marker as infowindow contents
-        marker.snippet(String.format("%.4f", point.latitude)+","+ String.format("%.4f", point
+        marker.snippet(String.format("%.4f", point.latitude) + "," + String.format("%.4f", point
                 .longitude));
 
         AlertDialog alertDialog = new AlertDialog.Builder(MapsActivity.this).create();
@@ -237,17 +338,37 @@ public class MapsActivity extends FragmentActivity implements
         polylineOptions.color(Color.RED);
         polylineOptions.width(5);
 
+        // TODO: distance of each line segment if needed.
+        //double distance = SphericalUtil.computeDistanceBetween(pointA.getPosition(), pointB.getPosition());
+
         // Adding points to array to draw line and add line to google map
         arrayPoints.add(point);
 
         polylineOptions.addAll(arrayPoints);
-        mMap.addPolyline(polylineOptions);
+        polyline = mMap.addPolyline(polylineOptions);
+        polyline.setClickable(true);
+    }
+
+    private String formatNumber(double distance) {
+        String unit = "m";
+        if (distance < 1) {
+            distance *= 1000;
+            unit = "mm";
+        } else if (distance > 1000) {
+            distance /= 1000;
+            unit = "km";
+        }
+
+        return String.format("%4.3f%s", distance, unit);
     }
 
     @Override
     public void onMapLongClick(LatLng point) {
         mMap.clear();
-        //arrayPoints.clear();
+        arrayPoints.clear();
+        distance = 0.0;
+        polyline_distance.setText("Distance: " +
+                formatNumber(distance));
     }
 
     /**
@@ -268,13 +389,6 @@ public class MapsActivity extends FragmentActivity implements
             }
             return false;
         }
-        return true;
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_items, menu);
         return true;
     }
 
